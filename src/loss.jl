9a43e0d6664d39aa05e487cea807130a811d3372
HedwigNordlinder <hedwignordlinder@gmail.com>
2025-09-08 16:24:40 +0200

    Switching process

--- src/loss.jl
+++ src/loss.jl
@@ -73,3 +73,6 @@ floss(P::fbu(SwitchingBM), X̂₁, X₁::msu(ContinuousState), c) =
     scaledmaskedmean(mse(X̂₁, X₁), c, getlmask(X₁))
 floss(P::fbu(SDEProcess), X̂₁, X₁::msu(ContinuousState), c) =
     scaledmaskedmean(mse(X̂₁, X₁), c, getlmask(X₁))
+# Add loss for SwitchingSDEProcess: predict continuous endpoint only; Q not learned
+floss(P::fbu(SwitchingSDEProcess), X̂₁, X₁::msu(ContinuousState), c) =
+    scaledmaskedmean(mse(X̂₁, X₁), c, getlmask(X₁))
